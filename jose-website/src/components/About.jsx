6194092251c0c36b5bd8810da65bd7e6b076bed9
Jose Rodolfo Nieblas <joserodolfonieblas@gmail.com>
2024-01-23 23:17:25 -0800

    Fixed Issue
    Deleted bad file

---
+++ jose-website/src/components/About.jsx
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from "react"
+import profilePic from "../assets/me.jpg"
+
+function About() {
+
+    const [currentAge, setTime] = useState(0); 
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTime(Math.abs(new Date() - new Date("1999-01-31T00:00:00")) / 31536000000);
+        }, 1);
+        return () => clearInterval(interval);
+    }, []);
+
+    return(
+        <>
+            <div id="profile">
+                <img id="profile-pic" src={profilePic} />
+                <h1 id="profile-title">Software Developer</h1>
+                <p id="bio">I am currently <span id="currAge">{currentAge.toFixed(9)}</span> years old and a recent graduate from 
+                Fresno State University with a Bachelor's in Computer Science. 
+                I grew up in Dinuba, CA where my early years were marked by challenging jobs in agriculture and factories, molding me into a resilient individual.
+                Now seeking a full-time role in the software industry, I bring a diverse background, coupled with genuine enthusiasm for contributing to innovative projects. 
+                I am eager to contribute my skills and dedication to a collaborative and forward-thinking team.
+                </p>
+                <a id="msg" href="mailto: joserodolfonieblas@gmail.com">Send Message</a>
+            </div>
+        </>
+    )
+
+    
+}
+
+export default About
---
+++ jose-website/src/components/Navbar.jsx
@@ -0,0 +1,87 @@
+import React, {useState} from "react"
+import pdf from "../assets/JoseNieblasResume.pdf"
+import arrow from "../assets/arrow.png"
+import linkedInLogo from "../assets/linkedin_logo.png"
+import githubLogo from "../assets/githubLogo.png"
+import emailLogo from "../assets/email_logo.png"
+
+function Navbar() {
+
+    const [navElements, setNav] = useState({about: true, p1: false, p2: false, p3: false});
+   
+    const lightBlue = {
+        color: '#147efb'
+    };
+
+    const white = {
+        color: '#FDFFF5'
+    };
+
+    return(
+        <section id="nav-wrapper">
+            <h2 id="my-name">Jose Rodolfo Nieblas</h2>
+            <nav>
+                <div id="nav-links-wrapper">
+                    <div className="nav-element-wrapper">
+                        <img id="nav-arrow-1" src={arrow} alt="arrow selector"/>
+                        <a 
+                            className="nav-element" 
+                            href={pdf} 
+                            target="_blank" 
+                            rel="noreferrer">
+                        Grab Resume</a>
+                    </div>
+                    <div className="nav-element-wrapper">
+                        <img id="nav-arrow-2" src={arrow} alt="arrow selector"/>
+                        <a 
+                            onClick={() => setNav({about: true, p1: false, p2: false, p3: false})}
+                            style={navElements.about === true ? white : lightBlue} 
+                            className="nav-element" 
+                            href="#profile">
+                        About Me</a>
+                    </div>
+                    <div className="nav-element-wrapper">
+                        <img id="nav-arrow-3" src={arrow} alt="arrow selector"/>
+                        <a 
+                            onClick={() => setNav({about: false, p1: true, p2: false, p3: false})}
+                            style={navElements.p1 === true ? white : lightBlue} 
+                            className="nav-element" 
+                            href="#p1">
+                        Project 1</a>
+                    </div>
+                    <div className="nav-element-wrapper">
+                        <img id="nav-arrow-4" src={arrow} alt="arrow selector"/>
+                        <a 
+                            onClick={() => setNav({about: false, p1: false, p2: true, p3: false})}
+                            style={navElements.p2 === true ? white : lightBlue} 
+                            className="nav-element" 
+                            href="#p2">
+                        Project 2</a>
+                    </div>
+                    <div className="nav-element-wrapper">
+                        <img id="nav-arrow-5" src={arrow} alt="arrow selector"/>
+                        <a 
+                            onClick={() => setNav({about: false, p1: false, p2: false, p3: true})}
+                            style={navElements.p3 === true ? white : lightBlue} 
+                            className="nav-element" 
+                            href="#p3">
+                        Project 3</a>
+                    </div>
+                </div>
+                <div id="nav-element-logos">
+                    <a href="https://www.linkedin.com/in/jose-nieblas-b96b4a25a/" target="_blank" rel="noreferrer">
+                        <img className="nav-logos" src={linkedInLogo} alt="LinkedIn Logo"/>
+                    </a>
+                    <a href="https://github.com/jdofa" target="_blank" rel="noreferrer">
+                        <img className="nav-logos" src={githubLogo} alt="GitHub Logo"/>
+                    </a>
+                    <a href="mailto: joserodolfonieblas@gmail.com">
+                        <img className="nav-logos" src={emailLogo} alt="Email Logo"/>
+                    </a>
+                </div>
+            </nav>
+        </section>
+    )
+}
+
+export default Navbar
