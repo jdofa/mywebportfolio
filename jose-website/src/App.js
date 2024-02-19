003906e95e92dae7f1dba4d512755e58fb916bbd
Jose Rodolfo Nieblas <joserodolfonieblas@gmail.com>
2024-02-18 20:00:40 -0800

    New Resume and Project Descriptions
    - Updated Resume
    - Updated project descriptions

--- jose-website/src/App.js
+++ jose-website/src/App.js
@@ -17,31 +17,31 @@ function App() {
       <section id="content">
         <About />
         <Project 
-          title="License Plate Scanner"
+          title="PlateMate"
           coverImg = {p1_img}
-          description="This project was created for my Sofware Engineering class, where I was the frontend developer. 
-          This project aimed to digitize parking permits, where the user would register in our website and then parking 
-          enforcer's would scan the user's license plate to see if they had a valid permit."
+          description="This project was created for my Sofware Engineering class, where I was the front-end developer. 
+          This project aims to digitize parking permits, where a user registers in our website and buys a permit. Then 
+          enforcer's would scan the user's license plate to see if they bought and have a non-expired, valid permit."
           link="https://github.com/150-lab2/License_Plate_Scanner"
-          tools={["JavaScript", "CSS3", "HTML5", "Figma", "UML", "SRS", "SDD", "Git", "Waterfall Model"]}
+          tools={["JavaScript", "CSS", "HTML", "Figma", "UML", "SRS", "SDD", "Git", "Waterfall Model", "Firebase"]}
           anchor="p1"
         />
         <Project 
           title="Checkers Web App"
           coverImg = {p2_img}
-          description="A web app that allows users to play checkers against each other locally or against an AI. User data is stored
-          and displayed on a global and local leaderboard. User's can also register and login to save their game progress."
+          description="A web app that allows users to play checkers against each other locally or against a bot. User data is stored
+          and displayed on a global and local leaderboards. User's can also register and login to save their game progress to both leaderboards."
           link="https://github.com/jdofa/Checkers130"
-          tools={["Javascript", "DOM API", "PHP", "MySQL", "Apache", "CSS3", "HTML5", "Session & Cookies"]}
+          tools={["Javascript", "DOM API", "PHP", "MySQL", "Apache", "CSS", "HTML", "Session & Cookies"]}
           anchor="p2"
         />
         <Project 
           title="Chat Room"
           coverImg = {p3_img}
-          description="A chat room made with Python3 that allows users to connect to a server where they
-          can send messages to anyone connected to it, in real time."
+          description="A chat room made with Python. A server is created and users can connect to it and chat with each other, but 
+          is limited to users on the same network."
           link="https://github.com/jdofa/BreakoutRoom"
-          tools={["Python3", "Threading", "Web Sockets"]}
+          tools={["Python", "Threading", "Web Sockets"]}
           anchor="p3"
         />
       </section>
